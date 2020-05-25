30a01cb5e99441cdba6e7320a99261804ac979c7
Ramiro Belluardo <ramirobelluardo1993@gmail.com>
2020-05-25 20:31:48 -0300

    Cambio a ResponseEntity

--- src/main/java/unq/dapp/ComprandoEnCasa/services/commerce/CommerceService.java
+++ src/main/java/unq/dapp/ComprandoEnCasa/services/commerce/CommerceService.java
@@ -1,13 +1,11 @@
 package unq.dapp.ComprandoEnCasa.services.commerce;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import unq.dapp.ComprandoEnCasa.model.domain.commerce.Commerce;
 import unq.dapp.ComprandoEnCasa.persistence.commerce.CommerceRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CommerceService {
@@ -16,17 +14,16 @@ public class CommerceService {
     private CommerceRepository repository;
 
     @Transactional
-    public List<Commerce> findAll() {
-        return repository.findAll();
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok().body(repository.findAll());
     }
 
     @Transactional
-    public Optional findById(int id) {
-        return repository.findById(id);
+    public ResponseEntity<?> findById(int id) {
+        return ResponseEntity.ok().body(repository.findById(id));
     }
 
-
-   @Transactional
+    @Transactional
     public void save(Commerce commerce) {
         this.repository.save(commerce);
     }
