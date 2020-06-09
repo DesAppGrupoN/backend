cdfa56dacac034edeebfef8c6a9d599664b27357
Ramiro Belluardo <ramirobelluardo1993@gmail.com>
2020-06-09 16:45:43 -0300

    Cambio CrossOrigin

--- src/main/java/unq/dapp/ComprandoEnCasa/webService/commerce/CommerceController.java
+++ src/main/java/unq/dapp/ComprandoEnCasa/webService/commerce/CommerceController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "", allowedHeaders = "")
+@CrossOrigin
 @RequestMapping("/commerce")
 public class CommerceController {
 
