d3b7e92c280427b5484d4835d2e94f579d15b452
Ramiro Belluardo <ramirobelluardo1993@gmail.com>
2020-07-25 15:33:21 -0300

    Validacion Commerce

--- src/main/java/unq/dapp/ComprandoEnCasa/model/domain/commerce/Commerce.java
+++ src/main/java/unq/dapp/ComprandoEnCasa/model/domain/commerce/Commerce.java
@@ -1,14 +1,6 @@
 package unq.dapp.ComprandoEnCasa.model.domain.commerce;
 
-
-import org.hibernate.annotations.Cascade;
-import org.hibernate.validator.constraints.Range;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.data.repository.cdi.Eager;
-
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
@@ -18,15 +10,13 @@ public class Commerce {
     @GeneratedValue
     private int id;
 
-    @NotBlank(message = "Name is mandatory")
     private String name;
 
     private String description;
 
     @Enumerated(EnumType.STRING)
-    @NotNull(message = "sector is mandatory")
     private Sector sector;
-    @NotBlank(message = "Address is mandatory")
+
     private String address;
 
     private String image;
@@ -35,8 +25,6 @@ public class Commerce {
     @CollectionTable
     @Enumerated(EnumType.STRING)
     private List<PayMethods> payMethods;
-    @NotNull(message = "maxDistance is mandatory")
-    @Range(min = 0)
     private Integer maxDistance;
 
     @OneToOne(cascade = CascadeType.ALL)
--- src/main/java/unq/dapp/ComprandoEnCasa/webService/CommerceController.java
+++ src/main/java/unq/dapp/ComprandoEnCasa/webService/CommerceController.java
@@ -1,13 +1,17 @@
 package unq.dapp.ComprandoEnCasa.webService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import unq.dapp.ComprandoEnCasa.model.domain.commerce.Commerce;
 import unq.dapp.ComprandoEnCasa.model.dtos.CommerceDTO;
 import unq.dapp.ComprandoEnCasa.services.CommerceService;
 
-import javax.validation.Valid;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,7 +22,6 @@ public class CommerceController {
 
     @Autowired
     private CommerceService commerceService;
-    private MyResourceNotFoundException myResourceNotFoundException= new MyResourceNotFoundException();
 
     @RequestMapping(value = "/get_all", method = RequestMethod.GET)
     public ResponseEntity<?> findAll() {
@@ -33,9 +36,23 @@ public class CommerceController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<String> addCommerce(@Valid @RequestBody CommerceDTO commerceDTO) {
-        commerceService.save(commerceDTO);
-        return ResponseEntity.ok("Commerce is valid");
+    public ResponseEntity<String> addCommerce(@RequestBody CommerceDTO commerceDTO) {
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        LocalTime openingTime = commerceDTO.getAttentionSchedule().getOpeningTime();
+        LocalTime closingTime = commerceDTO.getAttentionSchedule().getClosingTime();
+        List<DayOfWeek> openingDays = commerceDTO.getAttentionSchedule().getDays();
+
+        if( openingTime.isBefore(closingTime) && openingDays.size()>0) {
+            commerceService.save(commerceDTO);
+            return ResponseEntity.ok("Commerce is valid");
+        }
+        else {
+            return new ResponseEntity<String>(
+                    "Please, verify that the data is correct",
+                    headers,
+                    HttpStatus.BAD_REQUEST
+            );
+        }
     }
 
     @PostMapping(value = "/delete")
